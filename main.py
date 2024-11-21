24f7ab9f8005d71c7b7ccbc6cef05eb2d5c13566
christybad <christy.badila@laetottenham.ac.uk>
2024-11-21 09:55:06 +0000

    hello

---
+++ main.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def set_details(name,age,gender):
+
+    def get_details():
+        return('name:' + self.name + 'age: ' + self.age +  'gender: ' + self.gender)
+
+    personn = Person('Christy','17','F')
